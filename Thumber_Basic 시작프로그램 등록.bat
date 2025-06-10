ebc202920b6bbb73e6275321c66913cb61fc5807
pingoflimbs <pingoflimbs@gmail.com>
2025-06-10 21:55:51 +0900

    시작프로그램 등록하는코드 변경

--- Thumber_Basic 시작프로그램 등록.bat
+++ Thumber_Basic 시작프로그램 등록.bat
@@ -11,6 +11,7 @@ set AHK2EXE="%ProgramFiles%\AutoHotkey\Compiler\Ahk2Exe.exe"
 set SCRIPT_DIR=%~dp0
 set TARGET_DIR=%APPDATA%\ThumberAHK
 set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+set STARTUP_FOLDER_COMMON=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
 
 :: 대상 폴더 생성
 if not exist "%TARGET_DIR%" (
@@ -22,12 +23,15 @@ if not exist "%TARGET_DIR%" (
 %AHK2EXE% /in "%SCRIPT_DIR%%AHK_FILE%" /out "%SCRIPT_DIR%%EXE_NAME%"
 if not exist "%SCRIPT_DIR%%EXE_NAME%" (
     echo 컴파일 실패. 경로를 확인하세요.
-    pause
-    exit /b 1
+    pause        
 )
+
+:: [1/4] AHK 강제종료
+taskkill /f /im %EXE_NAME% /t >nul 2>&1
 
 :: [2/4] %APPDATA%\Thumber\로 복사
 :: echo [2/4] Copying to %TARGET_DIR% ...
+del /Y "%TARGET_DIR%\%EXE_NAME%" >nul 2>&1
 copy /Y "%SCRIPT_DIR%%EXE_NAME%" "%TARGET_DIR%\%EXE_NAME%"
 
 :: [3/4] 바로가기 생성 (복사된 파일 기준)
@@ -41,7 +45,10 @@ powershell -noprofile -command ^
 :: [4/4] 시작프로그램 폴더에 바로가기 복사
 :: echo [4/4] Registering shortcut to startup ...
 copy /Y "%SCRIPT_DIR%%SHORTCUT_NAME%" "%STARTUP_FOLDER%\%SHORTCUT_NAME%"
+copy /Y "%SCRIPT_DIR%%SHORTCUT_NAME%" "%STARTUP_FOLDER_COMMON%\%SHORTCUT_NAME%"
 del "%SCRIPT_DIR%%SHORTCUT_NAME%" >nul 2>&1
+
+start "" "%TARGET_DIR%\%EXE_NAME%"
 
 echo.
 echo.
