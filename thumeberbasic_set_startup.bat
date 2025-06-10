5a3a0ab01d56556cdbd67b87b14669039544380a
pingoflimbs <pingoflimbs@gmail.com>
2025-06-10 14:39:44 +0900

    썸버 스타트업 프로그램 등록 배치파일 수정, 실제 실행파일은 AppData/ThumberAHK 에 넣도록 변경

--- thumeberbasic_set_startup.bat
+++ thumeberbasic_set_startup.bat
@@ -1,39 +1,60 @@
 @echo off
 setlocal
 
-:: 경로 설정
+:: 파일 및 폴더 설정
 set AHK_FILE=ThumberBasic.ahk
-set EXE_FILE=ThumberBasic.exe
+set EXE_NAME=ThumberBasic.exe
 set SHORTCUT_NAME=ThumberBasic.lnk
+
+:: 경로 설정
+set AHK2EXE="%ProgramFiles%\AutoHotkey\Compiler\Ahk2Exe.exe"
+set SCRIPT_DIR=%~dp0
+set TARGET_DIR=%APPDATA%\ThumberAHK
 set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 
-:: AHK 컴파일러 경로 (AutoHotkey v1 기준)
-set AHK2EXE="%ProgramFiles%\AutoHotkey\Compiler\Ahk2Exe.exe"
+:: 대상 폴더 생성
+if not exist "%TARGET_DIR%" (
+    mkdir "%TARGET_DIR%"
+)
 
-:: 현재 스크립트 위치
-set SCRIPT_DIR=%~dp0
-
-:: Thumber.exe 생성
-echo [1/3] Compiling %AHK_FILE% ...
-%AHK2EXE% /in "%SCRIPT_DIR%%AHK_FILE%" /out "%SCRIPT_DIR%%EXE_FILE%"
-if not exist "%SCRIPT_DIR%%EXE_FILE%" (
+:: [1/4] AHK 컴파일
+:: echo [1/4] Compiling %AHK_FILE% ...
+%AHK2EXE% /in "%SCRIPT_DIR%%AHK_FILE%" /out "%SCRIPT_DIR%%EXE_NAME%"
+if not exist "%SCRIPT_DIR%%EXE_NAME%" (
     echo 컴파일 실패. 경로를 확인하세요.
     pause
     exit /b 1
 )
 
-:: 바로가기 생성 (PowerShell 사용)
-echo [2/3] Creating shortcut ...
-powershell -noprofile -command "$s = (New-Object -COM WScript.Shell).CreateShortcut('%SCRIPT_DIR%%SHORTCUT_NAME%'); $s.TargetPath = '%SCRIPT_DIR%%EXE_FILE%'; $s.WorkingDirectory = '%SCRIPT_DIR%'; $s.Save()"
+:: [2/4] %APPDATA%\Thumber\로 복사
+:: echo [2/4] Copying to %TARGET_DIR% ...
+copy /Y "%SCRIPT_DIR%%EXE_NAME%" "%TARGET_DIR%\%EXE_NAME%"
 
+:: [3/4] 바로가기 생성 (복사된 파일 기준)
+:: echo [3/4] Creating shortcut ...
+powershell -noprofile -command ^
+  "$s = (New-Object -COM WScript.Shell).CreateShortcut('%SCRIPT_DIR%%SHORTCUT_NAME%');" ^
+  "$s.TargetPath = '%TARGET_DIR%\%EXE_NAME%';" ^
+  "$s.WorkingDirectory = '%TARGET_DIR%';" ^
+  "$s.Save()"
 
-:: 시작프로그램 폴더로 복사
-echo [3/3] Registering to startup ...
+:: [4/4] 시작프로그램 폴더에 바로가기 복사
+:: echo [4/4] Registering shortcut to startup ...
 copy /Y "%SCRIPT_DIR%%SHORTCUT_NAME%" "%STARTUP_FOLDER%\%SHORTCUT_NAME%"
-
-
 del "%SCRIPT_DIR%%SHORTCUT_NAME%" >nul 2>&1
 
-
-echo Complete
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo ThumberBasic is registered as STARTUP
+echo.
+echo.
+echo.
+echo.
 pause
