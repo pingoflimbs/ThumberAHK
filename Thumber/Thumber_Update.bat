5b2b692a9476a0d6295f247e0e96977944f7e904
pingoflimbs <pingoflimbs@gmail.com>
2025-06-11 12:04:31 +0900

    기존 시작프로그램등록용 bat 수정하여 빌드,업데이트용으로 변경. 소스폴더에 뭐 안생기게 수정함

--- Thumber/Thumber_Update.bat
+++ Thumber/Thumber_Update.bat
@@ -3,7 +3,7 @@ setlocal
 
 set PRJ_NAME=Thumber
 
-:: íŒŒì¼ ë° í´ë” ì„¤ì •
+:: ÆÄÀÏ ¹× Æú´õ ¼³Á¤
 set AHK_FILE=%PRJ_NAME%.ahk
 set EXE_NAME=%PRJ_NAME%.exe
 set SHORTCUT_NAME=%PRJ_NAME%.lnk
@@ -11,59 +11,120 @@ set IMG_NAME=%PRJ_NAME%.png
 set OFF_AHK_FILE=%PRJ_NAME%_Off.ahk
 set OFF_EXE_NAME=%PRJ_NAME%_Off.exe
 
-:: ê²½ë¡œ ì„¤ì •
+:: °æ·Î ¼³Á¤
 set AHK2EXE="%ProgramFiles%\AutoHotkey\Compiler\Ahk2Exe.exe"
 set SCRIPT_DIR=%~dp0
 set TARGET_DIR=%APPDATA%\ThumberAHK\%PRJ_NAME%
-set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
-set STARTUP_FOLDER_COMMON=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
+set TMP_DIR=%APPDATA%\ThumberAHK\%PRJ_NAME%\Temp
+:: set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+:: set STARTUP_FOLDER_COMMON=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
 
-:: ëŒ€ìƒ í´ë” ìƒì„±
+
+
+echo --0-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo ´ë»ó Æú´õ »ý¼º
 if not exist "%TARGET_DIR%" (
     mkdir "%TARGET_DIR%"
 )
-
-:: AHK ì»´íŒŒì¼
-%AHK2EXE% /in "%SCRIPT_DIR%%AHK_FILE%" /out "%SCRIPT_DIR%%EXE_NAME%"
-if not exist "%SCRIPT_DIR%%EXE_NAME%" (
-    echo ì»´íŒŒì¼ ì‹¤íŒ¨. ê²½ë¡œë¥¼ í™•ì¸í•˜ì„¸ìš”.
-    pause        
+if not exist "%TMP_DIR%" (
+    mkdir "%TMP_DIR%"
 )
-:: OFF AHK ì»´íŒŒì¼
-%AHK2EXE% /in "%SCRIPT_DIR%%OFF_AHK_FILE%" /out "%SCRIPT_DIR%%OFF_EXE_NAME%"
-if not exist "%SCRIPT_DIR%%OFF_EXE_NAME%" (
-    echo ì»´íŒŒì¼ ì‹¤íŒ¨. ê²½ë¡œë¥¼ í™•ì¸í•˜ì„¸ìš”.
+echo.
+echo.
+echo TMP Æú´õ Á¤¸®
+if exist "%TMP_DIR%" (    
+    rmdir /s /q "%TMP_DIR%"
+)
+mkdir "%TMP_DIR%"
+
+
+
+echo -1-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo AHK ÄÄÆÄÀÏ
+%AHK2EXE% /in "%SCRIPT_DIR%%AHK_FILE%" /out "%TMP_DIR%\%EXE_NAME%"
+if not exist "%TMP_DIR%\%EXE_NAME%" (
+    echo No File Exist %EXE_NAME%
     pause
 )
+if errorlevel 1 (
+    echo  %EXE_NAME% Build Fail !
+    pause
+) else (
+    echo %EXE_NAME% Compile Success!
+)
 
-:: AHK ê°•ì œì¢…ë£Œ
+echo OFF AHK ÄÄÆÄÀÏ
+%AHK2EXE% /in "%SCRIPT_DIR%%OFF_AHK_FILE%" /out "%TMP_DIR%\%OFF_EXE_NAME%"
+if not exist "%TMP_DIR%\%EXE_NAME%" (
+    echo No File Exist %OFF_EXE_NAME%
+    pause
+)
+if errorlevel 1 (
+    echo  %OFF_EXE_NAME% Build Fail !
+    pause
+) else (
+    echo %OFF_EXE_NAME% Compile Success!
+)
+
+
+
+echo -2-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo AHK °­Á¦Á¾·á
 taskkill /f /im %EXE_NAME% /t >nul 2>&1
 
-:: APPDATA\Thumber\Thumber ë¡œ ë³µì‚¬ EXE,OFF,IMG
+timeout /t 2 > nul
+
+echo TMP¿¡¼­TARGETÀ¸·Î EXE º¹»ç
 del /Y "%TARGET_DIR%\%EXE_NAME%" >nul 2>&1
-copy /Y "%SCRIPT_DIR%%EXE_NAME%" "%TARGET_DIR%\%EXE_NAME%"
+copy /Y "%TMP_DIR%\%EXE_NAME%" "%TARGET_DIR%\%EXE_NAME%"
 
+echo TMP¿¡¼­TARGETÀ¸·Î OFF º¹»ç
 del /Y "%TARGET_DIR%\%OFF_EXE_NAME%" >nul 2>&1
-copy /Y "%SCRIPT_DIR%%OFF_EXE_NAME%" "%TARGET_DIR%\%OFF_EXE_NAME%"
+copy /Y "%TMP_DIR%\%OFF_EXE_NAME%" "%TARGET_DIR%\%OFF_EXE_NAME%"
 
+
+
+echo -3-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo ¸Å´º¾ó ÀÌ¹ÌÁöº¹»ç
 del /Y "%TARGET_DIR%\%IMG_NAME%" >nul 2>&1
 copy /Y "%SCRIPT_DIR%%IMG_NAME%" "%TARGET_DIR%\%IMG_NAME%"
 
 
-:: ë°”ë¡œê°€ê¸° ìƒì„± (ë³µì‚¬ëœ íŒŒì¼ ê¸°ì¤€)
+
+echo -4-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo ¹Ù·Î°¡±â »ý¼º (º¹»çµÈ ÆÄÀÏ ±âÁØ)
 powershell -noprofile -command ^
   "$s = (New-Object -COM WScript.Shell).CreateShortcut('%TARGET_DIR%\%SHORTCUT_NAME%');" ^
   "$s.TargetPath = '%TARGET_DIR%\%EXE_NAME%';" ^
   "$s.WorkingDirectory = '%TARGET_DIR%';" ^
   "$s.Save()"
 
-:: ì‹œìž‘í”„ë¡œê·¸ëž¨ í´ë”ì— ë°”ë¡œê°€ê¸° ë³µì‚¬
+
+
+::echo -5-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: ½ÃÀÛÇÁ·Î±×·¥ Æú´õ¿¡ ¹Ù·Î°¡±â º¹»ç
 :: del /Y "%STARTUP_FOLDER_COMMON%\%SHORTCUT_NAME%" >nul 2>&1
 :: copy /Y "%TARGET_DIR%\%SHORTCUT_NAME%" "%STARTUP_FOLDER_COMMON%\%SHORTCUT_NAME%"
 :: del /Y "%TARGET_DIR%\%SHORTCUT_NAME%" >nul 2>&1
 
-:: í”„ë¡œê·¸ëž¨ ì‹¤í–‰  
+
+echo -6-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo TMP Æú´õ Á¤¸®
+if exist "%TMP_DIR%" (    
+    rmdir /s /q "%TMP_DIR%"
+)
+echo ÇÁ·Î±×·¥ ½ÇÇà  
 start "" "%TARGET_DIR%\%EXE_NAME%"
 echo.
 echo.
+
+::pause
+timeout /t 8 > nul
 exit 0
--- ThumberBasic/ThumberBasic_Update.bat
+++ ThumberBasic/ThumberBasic_Update.bat
@@ -15,55 +15,116 @@ set OFF_EXE_NAME=%PRJ_NAME%_Off.exe
 set AHK2EXE="%ProgramFiles%\AutoHotkey\Compiler\Ahk2Exe.exe"
 set SCRIPT_DIR=%~dp0
 set TARGET_DIR=%APPDATA%\ThumberAHK\%PRJ_NAME%
-set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
-set STARTUP_FOLDER_COMMON=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
+set TMP_DIR=%APPDATA%\ThumberAHK\%PRJ_NAME%\Temp
+:: set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+:: set STARTUP_FOLDER_COMMON=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
 
-:: 대상 폴더 생성
+
+
+echo --0-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo 대상 폴더 생성
 if not exist "%TARGET_DIR%" (
     mkdir "%TARGET_DIR%"
 )
-
-:: AHK 컴파일
-%AHK2EXE% /in "%SCRIPT_DIR%%AHK_FILE%" /out "%SCRIPT_DIR%%EXE_NAME%"
-if not exist "%SCRIPT_DIR%%EXE_NAME%" (
-    echo 컴파일 실패. 경로를 확인하세요.
-    pause        
+if not exist "%TMP_DIR%" (
+    mkdir "%TMP_DIR%"
 )
-:: OFF AHK 컴파일
-%AHK2EXE% /in "%SCRIPT_DIR%%OFF_AHK_FILE%" /out "%SCRIPT_DIR%%OFF_EXE_NAME%"
-if not exist "%SCRIPT_DIR%%OFF_EXE_NAME%" (
-    echo 컴파일 실패. 경로를 확인하세요.
+echo.
+echo.
+echo TMP 폴더 정리
+if exist "%TMP_DIR%" (    
+    rmdir /s /q "%TMP_DIR%"
+)
+mkdir "%TMP_DIR%"
+
+
+
+echo -1-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo AHK 컴파일
+%AHK2EXE% /in "%SCRIPT_DIR%%AHK_FILE%" /out "%TMP_DIR%\%EXE_NAME%"
+if not exist "%TMP_DIR%\%EXE_NAME%" (
+    echo No File Exist %EXE_NAME%
     pause
 )
+if errorlevel 1 (
+    echo  %EXE_NAME% Build Fail !
+    pause
+) else (
+    echo %EXE_NAME% Compile Success!
+)
 
-:: AHK 강제종료
+echo OFF AHK 컴파일
+%AHK2EXE% /in "%SCRIPT_DIR%%OFF_AHK_FILE%" /out "%TMP_DIR%\%OFF_EXE_NAME%"
+if not exist "%TMP_DIR%\%EXE_NAME%" (
+    echo No File Exist %OFF_EXE_NAME%
+    pause
+)
+if errorlevel 1 (
+    echo  %OFF_EXE_NAME% Build Fail !
+    pause
+) else (
+    echo %OFF_EXE_NAME% Compile Success!
+)
+
+
+
+echo -2-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo AHK 강제종료
 taskkill /f /im %EXE_NAME% /t >nul 2>&1
 
-:: APPDATA\Thumber\Thumber 로 복사 EXE,OFF,IMG
+timeout /t 2 > nul
+
+echo TMP에서TARGET으로 EXE 복사
 del /Y "%TARGET_DIR%\%EXE_NAME%" >nul 2>&1
-copy /Y "%SCRIPT_DIR%%EXE_NAME%" "%TARGET_DIR%\%EXE_NAME%"
+copy /Y "%TMP_DIR%\%EXE_NAME%" "%TARGET_DIR%\%EXE_NAME%"
 
+echo TMP에서TARGET으로 OFF 복사
 del /Y "%TARGET_DIR%\%OFF_EXE_NAME%" >nul 2>&1
-copy /Y "%SCRIPT_DIR%%OFF_EXE_NAME%" "%TARGET_DIR%\%OFF_EXE_NAME%"
+copy /Y "%TMP_DIR%\%OFF_EXE_NAME%" "%TARGET_DIR%\%OFF_EXE_NAME%"
 
+
+
+echo -3-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo 매뉴얼 이미지복사
 del /Y "%TARGET_DIR%\%IMG_NAME%" >nul 2>&1
 copy /Y "%SCRIPT_DIR%%IMG_NAME%" "%TARGET_DIR%\%IMG_NAME%"
 
 
-:: 바로가기 생성 (복사된 파일 기준)
+
+echo -4-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo 바로가기 생성 (복사된 파일 기준)
 powershell -noprofile -command ^
   "$s = (New-Object -COM WScript.Shell).CreateShortcut('%TARGET_DIR%\%SHORTCUT_NAME%');" ^
   "$s.TargetPath = '%TARGET_DIR%\%EXE_NAME%';" ^
   "$s.WorkingDirectory = '%TARGET_DIR%';" ^
   "$s.Save()"
 
+
+
+::echo -5-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: 시작프로그램 폴더에 바로가기 복사
 :: del /Y "%STARTUP_FOLDER_COMMON%\%SHORTCUT_NAME%" >nul 2>&1
 :: copy /Y "%TARGET_DIR%\%SHORTCUT_NAME%" "%STARTUP_FOLDER_COMMON%\%SHORTCUT_NAME%"
 :: del /Y "%TARGET_DIR%\%SHORTCUT_NAME%" >nul 2>&1
 
-:: 프로그램 실행  
+
+echo -6-------------------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo TMP 폴더 정리
+if exist "%TMP_DIR%" (    
+    rmdir /s /q "%TMP_DIR%"
+)
+echo 프로그램 실행  
 start "" "%TARGET_DIR%\%EXE_NAME%"
 echo.
 echo.
+
+::pause
+timeout /t 8 > nul
 exit 0
