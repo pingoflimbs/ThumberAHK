7b34cd92d828dbb4c15a67ffd0ce5f2bcc6c2c0c
pingoflimbs <pingoflimbs@gmail.com>
2025-06-11 13:31:17 +0900

    사용자용 시작프로그램등록 배치파일 현재폴더에있는 Thumber.exe Thumber_Off.exe Thumber.png 세개의 파일을 AppData 폴더로옮기고 Thumber.lnk 를 만들어서 시작폴더에 넣음. 추후에 Thumber 의 GUI로 실행하게 할거임

---
+++ ThumberBasic/ThumberBasic_SetStartup.bat
@@ -0,0 +1,43 @@
+@echo off
+setlocal
+
+set "PRJ_NAME=ThumberBasic"
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+set "SRC_DIR=%~dp0"
+set "EXE_NAME=%PRJ_NAME%.exe"
+set "OFF_EXE_NAME=%PRJ_NAME%_Off.exe"
+set "IMG_NAME=%PRJ_NAME%.png"
+set "SHORTCUT_NAME=%PRJ_NAME%.lnk"
+
+set "TARGET_DIR=%APPDATA%\ThumberAHK\%PRJ_NAME%"
+set "STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo AHK task Kill
+taskkill /f /im %EXE_NAME% /t >nul 2>&1
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+if exist "%TARGET_DIR%" (
+    rmdir /s /q "%TARGET_DIR%"
+)
+mkdir "%TARGET_DIR%"
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+copy /Y "%SRC_DIR%%EXE_NAME%" "%TARGET_DIR%\"
+copy /Y "%SRC_DIR%%OFF_EXE_NAME%" "%TARGET_DIR%\"
+copy /Y "%SRC_DIR%%IMG_NAME%" "%TARGET_DIR%\"
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+powershell -noprofile -command ^
+  "$s = (New-Object -ComObject WScript.Shell).CreateShortcut('%TEMP%\%SHORTCUT_NAME%');" ^
+  "$s.TargetPath = '%TARGET_DIR%\%EXE_NAME%';" ^
+  "$s.WorkingDirectory = '%TARGET_DIR%';" ^
+  "$s.Save()"
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+copy /Y "%TEMP%\%SHORTCUT_NAME%" "%STARTUP_FOLDER%\"
+del /Q "%TEMP%\%SHORTCUT_NAME%" > nul
+
+echo [완료] 2초 후 종료됩니다...
+timeout /t 2 > nul
+exit 0
