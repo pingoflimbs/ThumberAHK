7bec49f670f863398b6abb11fc25faaff6379eae
pingoflimbs <pingoflimbs@gmail.com>
2025-06-11 11:27:28 +0900

    업데이트 배치파일 코드정리, Appdata폴더생성시 Thumber, ThumberBasic 폴더 따로 생성하게함

--- ThumberBasic/ThumberBasic_Update.bat
+++ ThumberBasic/ThumberBasic_Update.bat
@@ -1,18 +1,20 @@
 @echo off
 setlocal
 
+set PRJ_NAME=ThumberBasic
+
 :: 파일 및 폴더 설정
-set AHK_FILE=ThumberBasic.ahk
-set EXE_NAME=ThumberBasic.exe
-set SHORTCUT_NAME=ThumberBasic.lnk
-set IMG_NAME=ThumberBasic.png
-set OFF_AHK_FILE=ThumberBasic_Off.ahk
-set OFF_EXE_NAME=ThumberBasic_Off.exe
+set AHK_FILE=%PRJ_NAME%.ahk
+set EXE_NAME=%PRJ_NAME%.exe
+set SHORTCUT_NAME=%PRJ_NAME%.lnk
+set IMG_NAME=%PRJ_NAME%.png
+set OFF_AHK_FILE=%PRJ_NAME%_Off.ahk
+set OFF_EXE_NAME=%PRJ_NAME%_Off.exe
 
 :: 경로 설정
 set AHK2EXE="%ProgramFiles%\AutoHotkey\Compiler\Ahk2Exe.exe"
 set SCRIPT_DIR=%~dp0
-set TARGET_DIR=%APPDATA%\ThumberAHK
+set TARGET_DIR=%APPDATA%\ThumberAHK\%PRJ_NAME%
 set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 set STARTUP_FOLDER_COMMON=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
 
@@ -21,25 +23,23 @@ if not exist "%TARGET_DIR%" (
     mkdir "%TARGET_DIR%"
 )
 
-:: [1/4] AHK 컴파일
-:: echo [1/4] Compiling %AHK_FILE% ...
+:: AHK 컴파일
 %AHK2EXE% /in "%SCRIPT_DIR%%AHK_FILE%" /out "%SCRIPT_DIR%%EXE_NAME%"
 if not exist "%SCRIPT_DIR%%EXE_NAME%" (
     echo 컴파일 실패. 경로를 확인하세요.
     pause        
 )
-
+:: OFF AHK 컴파일
 %AHK2EXE% /in "%SCRIPT_DIR%%OFF_AHK_FILE%" /out "%SCRIPT_DIR%%OFF_EXE_NAME%"
 if not exist "%SCRIPT_DIR%%OFF_EXE_NAME%" (
     echo 컴파일 실패. 경로를 확인하세요.
     pause
 )
 
-:: [1/4] AHK 강제종료
+:: AHK 강제종료
 taskkill /f /im %EXE_NAME% /t >nul 2>&1
 
-:: [2/4] %APPDATA%\Thumber\로 복사
-:: echo [2/4] Copying to %TARGET_DIR% ...
+:: APPDATA\Thumber\Thumber 로 복사 EXE,OFF,IMG
 del /Y "%TARGET_DIR%\%EXE_NAME%" >nul 2>&1
 copy /Y "%SCRIPT_DIR%%EXE_NAME%" "%TARGET_DIR%\%EXE_NAME%"
 
@@ -49,35 +49,21 @@ copy /Y "%SCRIPT_DIR%%OFF_EXE_NAME%" "%TARGET_DIR%\%OFF_EXE_NAME%"
 del /Y "%TARGET_DIR%\%IMG_NAME%" >nul 2>&1
 copy /Y "%SCRIPT_DIR%%IMG_NAME%" "%TARGET_DIR%\%IMG_NAME%"
 
-:: [3/4] 바로가기 생성 (복사된 파일 기준)
-:: echo [3/4] Creating shortcut ...
+
+:: 바로가기 생성 (복사된 파일 기준)
 powershell -noprofile -command ^
   "$s = (New-Object -COM WScript.Shell).CreateShortcut('%TARGET_DIR%\%SHORTCUT_NAME%');" ^
   "$s.TargetPath = '%TARGET_DIR%\%EXE_NAME%';" ^
   "$s.WorkingDirectory = '%TARGET_DIR%';" ^
   "$s.Save()"
 
-:: [4/4] 시작프로그램 폴더에 바로가기 복사
-:: echo [4/4] Registering shortcut to startup ...
-:: copy /Y "%TARGET_DIR%\%SHORTCUT_NAME%" "%STARTUP_FOLDER%\%SHORTCUT_NAME%"
-del /Y "%STARTUP_FOLDER_COMMON%\%SHORTCUT_NAME%" >nul 2>&1
-copy /Y "%TARGET_DIR%\%SHORTCUT_NAME%" "%STARTUP_FOLDER_COMMON%\%SHORTCUT_NAME%"
-del /Y "%TARGET_DIR%\%SHORTCUT_NAME%" >nul 2>&1
+:: 시작프로그램 폴더에 바로가기 복사
+:: del /Y "%STARTUP_FOLDER_COMMON%\%SHORTCUT_NAME%" >nul 2>&1
+:: copy /Y "%TARGET_DIR%\%SHORTCUT_NAME%" "%STARTUP_FOLDER_COMMON%\%SHORTCUT_NAME%"
+:: del /Y "%TARGET_DIR%\%SHORTCUT_NAME%" >nul 2>&1
+
+:: 프로그램 실행  
 start "" "%TARGET_DIR%\%EXE_NAME%"
-
 echo.
 echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo ThumberBasic is registered as STARTUP
-echo.
-echo.
-echo.
-echo.
-
 exit 0
